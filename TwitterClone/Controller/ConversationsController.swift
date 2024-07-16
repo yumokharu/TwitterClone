1d5e302e8a4fe2c5737084349e4cf349b3c8b874
yumokharu <coyfm333@naver.com>
2024-07-16 18:24:45 +0900

    #1 Setting NavigationTitle

--- TwitterClone/Controller/ConversationsController.swift
+++ TwitterClone/Controller/ConversationsController.swift
@@ -17,8 +17,14 @@ class ConversationsController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemMint
+        configureUI()
     }
     
     // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        navigationItem.title = "Messages"
+        
+    }
 }
--- TwitterClone/Controller/NotificationController.swift
+++ TwitterClone/Controller/NotificationController.swift
@@ -17,8 +17,14 @@ class NotificationController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemCyan
+        configureUI()
     }
     
     // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        navigationItem.title = "Notifications"
+        
+    }
 }
